9b0780ac92519dc910b07b1b78cf686af158bbcc
Victor Garcia <victorgf2011@gmail.com>
2023-08-18 00:38:04 +0200

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log("vscode-langkeeper loaded");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-langkeeper" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "vscode-langkeeper.openConfig",
+    async () => {
+      const secrets = context.secrets;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-langkeeper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-langkeeper!');
-	});
+      let email: any = await secrets.get("email");
+      let password: any = await secrets.get("password");
 
-	context.subscriptions.push(disposable);
+	    console.log(email);
+
+      email = await vscode.window.showInputBox({
+        placeHolder: "Enter your email",
+        prompt: "Login into your account",
+	    	value: email
+      });
+
+      password = await vscode.window.showInputBox({
+        placeHolder: "Enter your password",
+        prompt: "Login into your account",
+		    value: password
+      });
+
+      vscode.window.showInformationMessage(
+        "Hello World from vscode-langkeeper!" + email + password
+      );
+
+	    await secrets.store("email", email);
+	    await secrets.store("password", password);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
