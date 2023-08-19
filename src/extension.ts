f8f882b2ad84c935a84f0505ee34eaa869aeaeae
Victor Garcia <victorgf2011@gmail.com>
2023-08-19 21:31:13 +0200

    feat: working login, languages pign

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,105 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import fetch from "node-fetch";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log("vscode-langkeeper loaded");
-
-  let disposable = vscode.commands.registerCommand(
+  let onStart = vscode.commands.registerCommand(
     "vscode-langkeeper.openConfig",
     async () => {
       const secrets = context.secrets;
 
+      let address: any = await secrets.get("address");
       let email: any = await secrets.get("email");
       let password: any = await secrets.get("password");
 
-	    console.log(email);
+      address = await vscode.window.showInputBox({
+        placeHolder: "Enter server address",
+        prompt: "Set server address",
+        value: address
+      });
 
       email = await vscode.window.showInputBox({
         placeHolder: "Enter your email",
         prompt: "Login into your account",
-	    	value: email
+        value: email
       });
 
       password = await vscode.window.showInputBox({
         placeHolder: "Enter your password",
         prompt: "Login into your account",
-		    value: password
+        value: password
       });
 
+      await secrets.store("address", address);
+      await secrets.store("email", email);
+      await secrets.store("password", password);
+
       vscode.window.showInformationMessage(
-        "Hello World from vscode-langkeeper!" + email + password
+        "Trying to login..."
       );
 
-	    await secrets.store("email", email);
-	    await secrets.store("password", password);
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+      const response = await fetch(`${address}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      });
+
+      const cookie = response.headers.get("set-cookie");
+      const token = cookie?.split(";")[0].split("=")[1];
+
+      if (token) {
+        await secrets.store("token", token);
+        vscode.window.showInformationMessage(
+          "Login successful"
+        );
+      }
+      else {
+        vscode.window.showErrorMessage(
+          "Login failed"
+        );
+      }
     }
   );
 
-  context.subscriptions.push(disposable);
+  let onSave = vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+    const token = await context.secrets.get("token");
+    if (!token) {
+      return;
+    }
+
+    const address = await context.secrets.get("address");
+    const extension = document.fileName.split(".").pop();
+
+    const response = await fetch(`${address}/languages/ping`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cookie": `id=${token}`
+      },
+      body: JSON.stringify({
+        extension,
+      }),
+    });
+
+    if (response.status !== 200) {
+      vscode.window.showErrorMessage(
+        "Langkeeper: Error " + await response.text()
+      );
+    }
+  });
+
+  context.subscriptions.push(onStart, onSave);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
